bb41fc2a188535e8158f5e72b5c445fd4e912115
Nicolas Barrows <contact@nicolasbarrows.com>
2023-08-01 09:21:48 -0500

    COMPLETE: Exercise 1

--- app.js
+++ app.js
@@ -6,6 +6,17 @@ console.log(
 // Exercise 1
 console.log("EXERCISE 1:\n==========\n");
 
+let a = 20;
+let b = 4;
+let add = a+b;
+let minus = a-b;
+let multiply = a*b;
+let dividing = a/b;
+console.log("a + b = " + add);
+console.log("a - b = " + minus);
+console.log("a * b = " + multiply);
+console.log("a / b = " + dividing);
+
 // YOUR CODE HERE
 
 // Exercise 2
